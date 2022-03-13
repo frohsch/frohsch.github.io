cf2d8e562a0fb937753f63c8e7a3802fd2128942
pinkpochacco <kminjoo1213@gmail.com>
2022-03-13 21:32:20 +0900

    1.3

--- pages/blog/guestbook.tsx
+++
@@ -1,9 +0,0 @@
-export async function getServerSideProps(context) {
-  const workers = process.env.NEXT_PUBLIC_WORKERS
-  const res = await fetch(encodeURI(`${workers}/guestbook`))
-  const data = await res.json()
-  const list = Object.keys(data).sort().reverse()
-  return {
-    props: { list, data },
-  }
-}
